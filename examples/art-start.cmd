211ac03cd12ea68149731662156ae3778e000c47
MartinJHammer <martin_hammer@live.dk>
2017-12-03 21:11:00 +0100

    Updated stuff.

--- examples/art-start.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-:: Starts a program. The "" prevents a bug.
-start "" "C:\Program Files\Autodesk\SketchBook\SketchBook.exe"
-start ""  www.sketchbook.com/blog/
---
+++ examples/home-pc-start.cmd
@@ -0,0 +1,39 @@
+@echo off
+
+:: The "" prevents a bug.
+
+::==========
+:: Drawing
+::==========
+
+:: Programgs
+start "" "C:\Program Files\Autodesk\SketchBook\SketchBook.exe"
+
+:: Blogs
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" www.sketchbook.com/blog/
+start "" thesecretstodrawing.com
+start "" www.pencildrawingmadeeasy.com
+
+:: Artists and galleries
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" rossdraws.deviantart.com/gallery/
+start "" kate-fox.deviantart.com/gallery/
+start "" sakimichan.deviantart.com/gallery/
+start "" nsio.deviantart.com/gallery/
+start "" kachima.deviantart.com/gallery/
+
+start "" ryky.deviantart.com/gallery/
+start "" zombiesmile.deviantart.com/gallery/
+start "" robotcatart.deviantart.com/gallery/
+start "" grooooovy.deviantart.com/gallery/
+start "" chacckco.deviantart.com/gallery/
+start "" edwinhuang.deviantart.com/gallery/
+start "" endling.deviantart.com/gallery/
+
+::==========
+:: Noise
+::==========
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" www.youtube.com/watch?v=9GEXJ8r5NVw :: Rain
+start "" www.youtube.com/watch?v=MB9U-rrblhY :: Hair dryer
