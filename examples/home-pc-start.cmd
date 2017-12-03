485088b181312d5c20339d0135f212fd73cf642e
MartinJHammer <martin_hammer@live.dk>
2017-12-03 21:36:38 +0100

    Changed site loading order.

--- examples/home-pc-start.cmd
+++ examples/home-pc-start.cmd
@@ -9,18 +9,6 @@
 :: Programgs
 start /wait "" "C:\Program Files\Autodesk\SketchBook\SketchBook.exe"
 
-:: Blogs
-start /wait "" "https://www.sketchbook.com/blog/"
-start /wait "" "http://thesecretstodrawing.com/"
-start /wait "" "http://www.pencildrawingmadeeasy.com/"
-
-::==========
-:: Noise
-::==========
-start /wait "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-start /wait "" "https://youtube.com/watch?v=9GEXJ8r5NVw" :: Rain
-start /wait "" "https://youtube.com/watch?v=MB9U-rrblhY" :: Hair dryer
-
 :: Artists and galleries
 start /wait "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 start /wait "" "https://rossdraws.deviantart.com/gallery/"
@@ -36,3 +24,15 @@ start /wait "" "https://grooooovy.deviantart.com/gallery/"
 start /wait "" "https://chacckco.deviantart.com/gallery/"
 start /wait "" "https://edwinhuang.deviantart.com/gallery/"
 start /wait "" "https://endling.deviantart.com/gallery/"
+
+:: Blogs
+start /wait "" "https://www.sketchbook.com/blog/"
+start /wait "" "http://thesecretstodrawing.com/"
+start /wait "" "http://www.pencildrawingmadeeasy.com/"
+
+::==========
+:: Noise
+::==========
+start /wait "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start /wait "" "https://youtube.com/watch?v=9GEXJ8r5NVw" :: Rain
+start /wait "" "https://youtube.com/watch?v=MB9U-rrblhY" :: Hair dryer
