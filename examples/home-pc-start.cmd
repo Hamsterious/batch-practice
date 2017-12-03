cc5f1655287d838e3024d847c18af88346710479
MartinJHammer <martin_hammer@live.dk>
2017-12-03 21:42:33 +0100

    Now it all works :)

--- examples/home-pc-start.cmd
+++ examples/home-pc-start.cmd
@@ -7,32 +7,32 @@
 ::==========
 
 :: Programgs
-start /wait "" "C:\Program Files\Autodesk\SketchBook\SketchBook.exe"
+start "" "C:\Program Files\Autodesk\SketchBook\SketchBook.exe"
 
 :: Artists and galleries
-start /wait "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-start /wait "" "https://rossdraws.deviantart.com/gallery/"
-start /wait "" "https://kate-fox.deviantart.com/gallery/"
-start /wait "" "https://sakimichan.deviantart.com/gallery/"
-start /wait "" "https://nsio.deviantart.com/gallery/"
-start /wait "" "https://kachima.deviantart.com/gallery/"
+start "" "https://rossdraws.deviantart.com/gallery/"
+start "" "https://kate-fox.deviantart.com/gallery/"
+start "" "https://sakimichan.deviantart.com/gallery/"
+start "" "https://nsio.deviantart.com/gallery/"
+start "" "https://kachima.deviantart.com/gallery/"
 
-start /wait "" "https://ryky.deviantart.com/gallery/"
-start /wait "" "https://zombiesmile.deviantart.com/gallery/"
-start /wait "" "https://robotcatart.deviantart.com/gallery/"
-start /wait "" "https://grooooovy.deviantart.com/gallery/"
-start /wait "" "https://chacckco.deviantart.com/gallery/"
-start /wait "" "https://edwinhuang.deviantart.com/gallery/"
-start /wait "" "https://endling.deviantart.com/gallery/"
+start "" "https://ryky.deviantart.com/gallery/"
+start "" "https://zombiesmile.deviantart.com/gallery/"
+start "" "https://robotcatart.deviantart.com/gallery/"
+start "" "https://grooooovy.deviantart.com/gallery/"
+start "" "https://chacckco.deviantart.com/gallery/"
+start "" "https://edwinhuang.deviantart.com/gallery/"
+start "" "https://endling.deviantart.com/gallery/"
 
 :: Blogs
-start /wait "" "https://www.sketchbook.com/blog/"
-start /wait "" "http://thesecretstodrawing.com/"
-start /wait "" "http://www.pencildrawingmadeeasy.com/"
+start /wait "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "https://www.sketchbook.com/blog/"
+start "" "http://thesecretstodrawing.com/"
+start "" "http://www.pencildrawingmadeeasy.com/"
 
 ::==========
 :: Noise
 ::==========
 start /wait "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-start /wait "" "https://youtube.com/watch?v=9GEXJ8r5NVw" :: Rain
-start /wait "" "https://youtube.com/watch?v=MB9U-rrblhY" :: Hair dryer
+start "" "https://youtube.com/watch?v=9GEXJ8r5NVw" :: Rain
+start "" "https://youtube.com/watch?v=MB9U-rrblhY" :: Hair dryer
