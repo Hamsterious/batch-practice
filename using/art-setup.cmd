de6a3e2b8b29a5fa41523b184e944e71548dc8c3
MartinJHammer <martin_hammer@live.dk>
2017-12-05 16:50:12 +0100

    Updated scripts.

---
+++ using/art-setup.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+::==========
+:: Drawing
+::==========
+
+:: Programgs
+start "" "C:\Program Files\Autodesk\SketchBook\SketchBook.exe"
+start "" "C:\Users\marti\Desktop\Dropbox\Art"
+start "" "D:\Art"
